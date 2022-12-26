f49055d4eee703eaa2f59b47233850c51d051493
Taylouni MHD Mulham <M.TAYLOUNI@htlkrems.at>
2022-12-26 17:18:59 +0100

    adding lock to singleton

--- Auction/Auction/LogWriter.cs
+++ Auction/Auction/LogWriter.cs
@@ -8,21 +8,24 @@ namespace Auction
 {
     internal class LogWriter
     {
+        // Singleton implementation
         private static StreamWriter sw;//Deklaration
         private static LogWriter Instance { get; set; } = null;
+        private static readonly object Lock = new object();
         public static LogWriter GetInstance
         {
-            get { 
-
-                if(Instance == null)
-                    Instance = new LogWriter();
-                return Instance;
+            get {
+                lock (Lock)
+                {
+                    if (Instance == null)
+                        Instance = new LogWriter();
+                    return Instance;
+                }
             }
         }
 
-        
-
         private LogWriter() {
+            //creating the log name automaticly
             string file = "Log_" + DateTime.Now.ToString().Replace('/', '_')
                 + DateTime.Now.Millisecond.ToString() + ".txt";
             sw = new StreamWriter(file.Replace(':', '_'));//Initialisierung
